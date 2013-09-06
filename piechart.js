3802060da0524b69a5be08060e705704dc88b5ab
dquenault <davidquenault@hotmail.com>
2013-09-06 21:46:57 +0100

    typekit on

---
+++ piechart.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+    data = [{"label":"Developer", "value":20}, 
+            {"label":"Solution Architect", "value":50}, 
+            {"label":"Enterprise Architect", "value":30}
+            {"label":"Education", "value":30}];
+    
+
+
+</script>
